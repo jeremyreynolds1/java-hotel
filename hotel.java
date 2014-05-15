bdf2b501f46752afe124c98b8b8b25f595f2e88d
Jeremy Reynolds <jreynolds323@gmail.com>
2014-05-15 13:30:35 -0500

    added hotelRoom class.

--- hotel.java
+++ hotel.java
@@ -1,11 +1,17 @@
 /*
 	Hotel reservation system.
 */
-
+import java.util.*;
 class hotel {
 	public static void main(String[] args) {
+		//Guest creation
 		Guest guest1 = new Guest("Jeremy", "Reynolds");
-		System.out.print(guest1.getFirst());
+		
+		//HotelRoom objects will go here
+		List hotelRooms = new ArrayList(40);
+		
+		//populate hotelRooms with random rooms.
+		
 	}
 }
 
@@ -23,5 +29,24 @@ class Guest{
 	public String getLast(){
 		return lastName;
 	}
-	//hello?
+	
+}
+class HotelRoom{
+	//type of room, cost, and who is it reserved to
+	String type;
+	double cost;
+	
+	public basicRoom(){
+		type = "basic";
+		cost = 50.0;
+	}
+	public mediumRoom(){
+		type = "medium";
+		cost = 100.0;
+	}
+	public deluxeRoom(){
+		type = "deluxe";
+		cost = 150.0;
+	}
+	
 }
