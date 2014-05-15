1cee111009466fe07ed0814676c77613516b061c
Jeremy Reynolds <jreynolds323@gmail.com>
2014-05-15 08:22:52 -0500

    added get methods for first and last name.

--- hotel.java
+++ hotel.java
@@ -5,15 +5,22 @@
 class hotel {
 	public static void main(String[] args) {
 		Guest guest1 = new Guest("Jeremy", "Reynolds");
+		System.out.print(guest1.getFirst());
 	}
 }
 
 class Guest{
 	private String firstName;
 	private String lastName;
-	public Guest(String firstName, String lastName){
-		firstName = firstName;
-		lastName = lastName;
+	public Guest(String first, String last){
+		firstName = first;
+		lastName = last;
 	}
 	//need to create get and set method to change later.
+	public String getFirst(){
+		return firstName;
+	}
+	public String getLast(){
+		return lastName;
+	}
 }
